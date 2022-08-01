e744fc3cb4b360226b80d5a148d53d554f084b9a
Ryeohwan <arh951019@gmail.com>
2022-08-02 01:40:22 +0900

    [2022-08-02 안려환] 거의 다 옴

--- src/안려환/P001_SWEA1289_원재의메모리복구하기.java
+++ src/안려환/P001_SWEA1289_원재의메모리복구하기.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class P001_SWEA1289_원재의메모리복구하기 {
 
 	public static void main(String[] args) {
-		char [] arr_1 = new char[51];
+		
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for(int j = 0;j<n;j++) {
+			char [] arr_1 = new char[51];
 			int cnt = 0;
 			String line = sc.next();
 			char check = '0';
