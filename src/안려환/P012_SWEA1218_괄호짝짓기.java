3655901e90fc55d57daeb692d47d2f57ee1d53b9
Ryeohwan <ahnrh951019@gmail.com>
2022-08-05 06:11:30 +0900

    [2022-08-05 안려환] 암호생성기까지 마무리

--- src/안려환/P012_SWEA1218_괄호짝짓기.java
+++ src/안려환/P012_SWEA1218_괄호짝짓기.java
@@ -15,7 +15,7 @@ public class P012_SWEA1218_괄호짝짓기 {
 		
 		for(int t = 1 ; t<11; t++) {
 			Stack<String> stack1 = new <String>Stack();
-			Stack<String> stack2 = new <String>Stack();
+			Stack<String> stack2 = new <String>Stack();  // 기호별 다른 스택을 사용한다.
 			Stack<String> stack3 = new <String>Stack();
 			Stack<String> stack4 = new <String>Stack();
 			StringBuilder sb = new StringBuilder();
@@ -23,9 +23,9 @@ public class P012_SWEA1218_괄호짝짓기 {
 			String []str = br.readLine().split("");  // char 단위로 받아낼 때 사용하자.
 			int check = 0;
 			for(int test = 0; test<n;test++) {
-				switch(str[test]) {
+				switch(str[test]) {  // 조건을 넣어준다.
 				case "(":
-					stack1.add(str[test]);
+					stack1.add(str[test]);  // 여는 괄호는 스택에 넣어둔다.
 					break;
 				case "{":
 					stack2.add(str[test]);
@@ -37,7 +37,7 @@ public class P012_SWEA1218_괄호짝짓기 {
 					stack4.add(str[test]);
 					break;
 				case ")":
-					if(stack1.size()>0) {
+					if(stack1.size()>0) {  // 닫는 괄호들은 스택 길이를 판별하여 꺼내준다. 없다면 check 에 1을 넣는다.
 						stack1.pop();
 						break;
 					}else {
@@ -71,10 +71,10 @@ public class P012_SWEA1218_괄호짝짓기 {
 				
 			}
 			
-			if(stack1.isEmpty() == false || stack2.isEmpty() == false || stack3.isEmpty() == false || stack4.isEmpty() == false) {
+			if(stack1.isEmpty() == false || stack2.isEmpty() == false || stack3.isEmpty() == false || stack4.isEmpty() == false) {  // 각 스택들이 비어있어야만 0이다.
 				check = 1;
 			}
-			if(check == 1) {
+			if(check == 1) {  // 생각해보니 닫는 괄호부터 튀어나오는거라 거꾸로 생각했어서 답을 거꾸로 내줬다.
 				check = 0;
 			}else {
 				check = 1;
--- src/안려환/P014_BJ2164_카드2.java
+++ src/안려환/P014_BJ2164_카드2.java
@@ -21,7 +21,7 @@ public class P014_BJ2164_카드2 {
 		
 		while(arr.size()!= 1) {  // size 가 1이 되면 마지막 남는 값이 됩니다.
 			arr.remove();  // 맨 앞의 값을 제거해줍니다.
-			arr.offer(arr.poll());  //맨 앞의 값을 제거하지 않고 가져와서 offer 를 이용하여 더해줍니다. 
+			arr.offer(arr.poll());  // 이번엔 맨 앞의 값을 제거하지 않고 가져와서 offer 를 이용하여 더해줍니다. 
 		}
 		System.out.println(arr.poll());  // 마지막 남은 값을 출력해줍니다.
 	}
