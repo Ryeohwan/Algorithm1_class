45958c5593669171bf15724e9b71179a7d001e58
Ryeohwan <ahnrh951019@naver.com>
2022-08-11 16:49:57 +0900

    [2022-08-11 안려환] 햄버거

---
+++ src/안려환/P029_SWEA5215_햄버거다이어트.java
@@ -0,0 +1,58 @@
+package 안려환;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P029_SWEA5215_햄버거다이어트 {
+	static int [] kal;
+	static int [] pref;
+	static int L;
+	static int max = 0;
+	static boolean [] isVisited;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int test = 1; test<T+1;test++) {
+			StringBuilder sb = new StringBuilder();
+			String [] nl = br.readLine().split(" ");
+			int N = Integer.parseInt(nl[0]);
+			L = Integer.parseInt(nl[1]); 
+			
+			kal = new int [N];
+			pref = new int [N];   // 칼로리와 선호도를 담을 배열
+			isVisited = new boolean[N];
+			for (int i = 0; i < N; i++) {
+				String [] kp = br.readLine().split(" ");
+				pref[i] = Integer.parseInt(nl[0]);
+				kal[i] = Integer.parseInt(nl[1]);
+			} // 입력부 마무리
+			
+			
+			sb.append("#").append(test).append(" ");
+			System.out.print(sb);
+			recursion(kal,pref,0,0,L);
+		}
+
+	}
+	
+	public static void recursion(int[] kal,int[] pref, int total_kal, int total_pref,  int L) {
+		if(total_kal > L) return;  // L 보다 작거나 같아야 재귀가 돌아간다. - > 돌리고 나서 바로 커지는거면 멈춰야 되는데
+		for (int i = 0; i < kal.length; i++) {
+			if(!isVisited[i]) {
+				if(total_kal + kal[i]> L) {
+					max = total_pref;
+				}else {
+					isVisited[i] = true;
+					recursion(kal,pref, total_kal + kal[i], total_pref + pref[i], L);
+					isVisited[i] = false;
+				}
+				
+			}
+		}
+		System.out.print(max);
+	}
+
+}
