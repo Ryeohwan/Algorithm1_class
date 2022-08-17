d5d62965e56b7a4b8e3d9519a24ea06bd48a94c2
YongWoon_Yun <yuyun0124@gmail.com>
2022-08-17 14:17:57 +0900

    [2022-07-19 윤용운] 잃어버린 괄호 완

---
+++ src/안려환/P039_BJ1541_잃어버린괄호.java
@@ -0,0 +1,25 @@
+package 안려환;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P039_BJ1541_잃어버린괄호 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("-");
+        int answer = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            String[] tmpArr = input[i].split("\\+");
+            for (String s : tmpArr) {
+                if (i == 0) {
+                    answer += Integer.parseInt(s);
+                } else {
+                    answer -= Integer.parseInt(s);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
