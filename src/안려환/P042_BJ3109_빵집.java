623b307429daf9ed178f9aff121f930cb9e87aa6
Ryeohwan <ahnrh951019@naver.com>
2022-08-18 17:48:08 +0900

    [2022-08-18 안려환] 빵집 아직

--- src/안려환/P042_BJ3109_빵집.java
+++ src/안려환/P042_BJ3109_빵집.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 public class P042_BJ3109_빵집 {
 	static char [][] map;
 	static int starti = 0, startj = 0, cnt = 0;
-	static int C;
+	static int R,C;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String [] arr = br.readLine().split(" ");
-		int R = Integer.parseInt(arr[0]);
+		R = Integer.parseInt(arr[0]);
 		C = Integer.parseInt(arr[1]);
 		map = new char [R][C];
 		for (int i = 0; i < R; i++) {
@@ -21,34 +21,49 @@ public class P042_BJ3109_빵집 {
 			}
 		} // 입력부
 		
+		setLocation(0,0);
+		
 	}
 	
 	public static int isAvailable(int starti,int startj) { // 건물과 이미 지나간 파이프를 X로 표시해서 int 로 방향지정
 		// 오른쪽, 오른쪽위 , 오른쪽 아래
-		if(map[starti][startj] != 'X') {
-			if(map[starti][startj+1] != 'X') return 1;
-			else if (map[starti+1][startj+1] != 'X') return 2;
-			else if (map[starti-1][startj+1] != 'X') return 3;
+		if(map[starti][startj] != 'X'||map[starti][startj] != 'z' && starti-1 > 0 && starti-1 < R && startj-1 >0 && startj-1 < R) {
+			if(map[starti-1][startj+1] != 'X') return 1;  // 대각선 위
+			else if (map[starti][startj+1] != 'X') return 2;  // 우측으로 이동
+			else if (map[starti+1][startj+1] != 'X') return 3; // 대각선 아래
 			else return 0;  // 0이 나오면 아래 줄로 이동해야 한다.
-		}else {  // 0,0에  건물이 있는 경우
+		}else if(map[starti][startj] == 'X'||map[starti][startj] == 'z'){  // 0,0에  건물이 있는 경우
 			 // 아래 칸으로 내려간다.
 			return 0; // 0이 나오면 아래 줄로 이동해야 한다.
+		}else {
+			return -1;
 		}
 		
 	}
 	
+	
 	public static void setLocation(int starti,int startj) {
+		map[starti][startj] = 'z';
 		if (startj == C) {
 			cnt ++;
-			starti = 0;
 			startj = 0;
 		}
 		switch(isAvailable(starti,startj)) {
+		case -1:
+			break;
 		case 0:
 			setLocation(starti+1,startj);
 			break;
 		case 1:
+			setLocation(starti-1,startj+1);
+		case 2:
 			setLocation(starti,startj+1);
+			break;
+		case 3:
+			setLocation(starti+1,startj+1);
+			break;
+		default:
+			break;
 		}
 	}
 
