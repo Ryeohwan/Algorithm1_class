361e2f4b40a3d76fd201692dca82fd0c2d32507e
Ryeohwan <ahnrh951019@naver.com>
2022-08-18 13:21:32 +0900

    [2022-08-18 안려환] 빵집 미완

--- src/안려환/A002_BJ1780_종이의_개수.java
+++ src/안려환/A002_BJ1780_종이의_개수.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class A002_BJ1780_종이의_개수 {
-
+	static int map[][];
+	static int starti,startj,N;
+	static int one = 0,zero = 0, minus = 0;
+	static StringBuilder sb  = new StringBuilder();
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int [][] map = new int [N][N];
+		map = new int [N][N];
 		for (int i = 0; i < N; i++) {
 			String [] arr = br.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
@@ -17,9 +20,45 @@ public class A002_BJ1780_종이의_개수 {
 		}
 		// 입력 부 
 		
-		
+		conquer(0,0,N);
+		System.out.println(one);
+		System.out.println(zero);
+		System.out.println(minus);
 	}
-	static class conquer{
+	static boolean check(int starti, int startj,int N){
+		int temp = map[starti][startj];
+		for (int i = starti; i < N + starti; i++) {
+			for (int j = startj; j < N + startj; j++) {
+				if(temp != map[starti][startj]) return true;
+				else {
+					if(map[starti][startj] == 1) {
+						one++;
+						return false;
+					}else if (map[starti][startj] == 0) {
+						zero++;
+						return false;
+					}else if (map[starti][startj] == -1) {
+						minus++;
+						return false;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	static void conquer(int starti, int startj,int N){
+		if(check(starti,startj,N)) {
+			conquer(starti,startj,N/3);
+			conquer(starti,startj+N/3,N/3);
+			conquer(starti,startj+2*N/3,N/3);
+			conquer(starti+N/3,startj,N/3);
+			conquer(starti+2*N/3,startj,N/3);
+			conquer(starti+N/3,startj+N/3,N/3);
+			conquer(starti+2*N/3,startj+2*N/3,N/3);
+		}else {
+			
+		}
 		
 	}
 }
---
+++ src/안려환/P042_BJ3109_빵집.java
@@ -0,0 +1,50 @@
+package 안려환;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P042_BJ3109_빵집 {
+	static char [][] map;
+	static int starti = 0, startj = 0;
+	static int C;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String [] arr = br.readLine().split(" ");
+		int R = Integer.parseInt(arr[0]);
+		C = Integer.parseInt(arr[1]);
+		map = new char [R][C];
+		for (int i = 0; i < R; i++) {
+			String arr1 = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i] = arr1.toCharArray();
+			}
+		} // 입력부
+		
+	}
+	
+	public static int isAvailable(int starti,int startj) { // 건물과 이미 지나간 파이프를 X로 표시해서 int 로 방향지정
+		// 오른쪽, 오른쪽위 , 오른쪽 아래
+		if(map[starti][startj] != 'X') {
+			if(map[starti][startj+1] != 'X') return 1;
+			else if (map[starti+1][startj+1] != 'X') return 2;
+			else if (map[starti-1][startj+1] != 'X') return 3;
+			else return 0;  // 0이 나오면 아래 줄로 이동해야 한다.
+		}else {  // 0,0에  건물이 있는 경우
+			 // 아래 칸으로 내려간다.
+			return 0; // 0이 나오면 아래 줄로 이동해야 한다.
+		}
+		
+	}
+	
+	public static void setLocation(int starti,int startj) {
+		if (startj == C)
+		switch(isAvailable(starti,startj)) {
+		case 1:
+			
+		}
+	}
+
+}
+
+
