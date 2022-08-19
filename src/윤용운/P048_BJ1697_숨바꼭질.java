12a3b17ed6002fe8eb5f9aff0143ce75cf801ae7
YongWoon_Yun <yuyun0124@gmail.com>
2022-08-19 16:28:12 +0900

    [2022-08-19 윤용운] 숨바꼭질 속도 개선

--- src/윤용운/P048_BJ1697_숨바꼭질.java
+++ src/윤용운/P048_BJ1697_숨바꼭질.java
@@ -1,66 +1,61 @@
 package 윤용운;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class P048_BJ1697_숨바꼭질 {
+
     static Queue<Node> q = new LinkedList<>();
     static boolean[] visited = new boolean[100001];
+    static int N, K;
 
-    static class Node{
-        int n,depth;
-        public Node(int n, int depth){
+    static class Node {
+
+        int n, depth;
+
+        public Node(int n, int depth) {
             this.n = n;
             this.depth = depth;
         }
     }
 
-    public static void main(String[] args) throws Exception{
-        int N,K;
+    public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        bfs(new Node(N,0),K);
+        bfs(new Node(N, 0));
     }
-    private static void bfs(Node node, int end) {
+
+    private static void bfs(Node node) {
         q.add(node);
         visited[node.n] = true;
-        while(true){
+        while (true) {
             Node thisNode = q.poll();
             int thisN = thisNode.n;
             int thisDepth = thisNode.depth;
 
-            if(thisN == end){
+            if (thisN == K) {
                 System.out.println(thisNode.depth);
                 break;
             }
 
-
-            if(thisN - 1 >= 0){
-                if(!visited[thisN - 1]){
-                    q.add(new Node(thisN - 1,thisDepth+1));
-                    visited[thisN - 1] = true;
-                }
+            if (thisN - 1 >= 0 && !visited[thisN - 1]) {
+                q.add(new Node(thisN - 1, thisDepth + 1));
+                visited[thisN - 1] = true;
             }
-            if(thisN + 1 <= 100000){
-                if(!visited[thisN + 1]){
-                    q.add(new Node(thisN + 1,thisDepth+1));
-                    visited[thisN + 1] = true;
-                }
+            if (thisN + 1 <= 100000 && !visited[thisN + 1]) {
+                q.add(new Node(thisN + 1, thisDepth + 1));
+                visited[thisN + 1] = true;
             }
-            if(thisN * 2 <= 100000){
-                if(!visited[thisN * 2]){
-                    q.add(new Node(thisN * 2,thisDepth+1));
-                    visited[thisN * 2] = true;
-                }
+            if (thisN * 2 <= 100000 && !visited[thisN * 2]) {
+                q.add(new Node(thisN * 2, thisDepth + 1));
+                visited[thisN * 2] = true;
             }
         }
     }
-
 }
